24d1d6bf7f506b135773de7355a5677b83074369
Victoria Terenina (torymur) <torymur@gmail.com>
2020-05-15 10:56:53 +0100

    [MR] ch13-4 criterion

---
+++ mastering_rust/criterion_demo/src/lib.rs
@@ -0,0 +1,27 @@
+pub fn slow_fibonacci(nth: usize) -> u64 {
+    if nth <= 1 {
+        return nth as u64;
+    } else {
+        return slow_fibonacci(nth - 1) + slow_fibonacci(nth - 2);
+    }
+}
+
+pub fn fast_fibonacci(nth: usize) -> u64 {
+    let mut a = 0;
+    let mut b = 1;
+    let mut c = 0;
+    for _ in 1..nth {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    c
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
