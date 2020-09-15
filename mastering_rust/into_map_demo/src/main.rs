a6675c3fa315f31f29c93f9063b641abe1f7e073
Victoria Terenina (torymur) <torymur@gmail.com>
2020-09-15 22:28:01 +0100

    [MR] ch9 proc macro

---
+++ mastering_rust/into_map_demo/src/main.rs
@@ -0,0 +1,18 @@
+use into_map_derive::IntoMap;
+
+#[derive(IntoMap)]
+struct User {
+    name: String,
+    id: usize,
+    active: bool,
+}
+
+fn main() {
+    let my_bar = User {
+        name: "Alice".to_string(),
+        id: 35,
+        active: false,
+    };
+    let map = my_bar.into_map();
+    println!("{:?}", map);
+}
